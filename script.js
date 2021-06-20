d35ee25ea8e2ce84493d05481ff464244908a5b5
Natalia Rodríguez Calvo <natalia.rodriguez.calvo@udc.es>
2021-06-20 05:20:06 +0200

    tercera version

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function getPicture(responseObject) {
 function getTypes(responseObject)   {
     let types = responseObject.types;
     let len = types.length;
-    let typeHtml = '<div id="types"><p>Tipos</p><table><tr></tr>';
+    let typeHtml = '<div class="info"><p>Tipos</p><table><tr></tr>';
     let i;
     for(i = 0; i<len; i++){
         typeHtml = typeHtml + '<td>' + types[i].type.name + '</td>';
@@ -19,7 +19,21 @@ function getTypes(responseObject)   {
 }
 
 function getAbilities(responseObject)   {
-    return responseObject.abilities;//Array de abilidades, ability.name para obtener nombre y isHidden para ver si es oculta
+    let abilities = responseObject.abilities;
+    let len = abilities.length;
+    let abilitiesHtml = '<div class="info"><p>Habilidades</p><table><tr></tr>';
+    let i;
+    let hidden;
+    for(i = 0; i<len; i++){
+        hidden = abilities[i].is_hidden;
+        if(hidden) {
+            abilitiesHtml = abilitiesHtml + '<td>' + abilities[i].ability.name + '<br>(hidden)</td>';
+        } else {
+            abilitiesHtml = abilitiesHtml + '<td>' + abilities[i].ability.name + '</td>';
+        }
+    }
+    abilitiesHtml = abilitiesHtml + '</tr></table></div>';
+    return abilitiesHtml;
 }
 
 function createHtml(responseObject) {
@@ -27,6 +41,7 @@ function createHtml(responseObject) {
     html = html + '<img height=500px width=500px src=' + getPicture(responseObject) + '>';
     html = html + '<p>' + capitalizeFirstLetter(responseObject.name) + '</p>';
     html = html + getTypes(responseObject);
+    html = html + getAbilities(responseObject);
     html = html + '</div>'
     return html;
 }
